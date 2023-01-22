4e40e3f1bd3a05d9c107262775e9a712ccbcf588
SambhavJain <sambhav.jain@live.com>
2023-01-22 21:21:03 +0530

    Amending .gitignore and _1BasicPostDemo files.

--- src/test/java/com/psrestassured/testscripts/PostImplementations/_1BasicPostDemo.java
+++ src/test/java/com/psrestassured/testscripts/PostImplementations/_1BasicPostDemo.java
@@ -11,20 +11,17 @@ import org.testng.annotations.Test;
 
 public class _1BasicPostDemo extends BaseTest {
 
-    @Test (dataProvider = "", dataProviderClass=BaseDataProvider.class)
+    @Test
     public void asUserWouldLikeToVerifyUserCreationUsingPojoObject(){
         
         User user=new User("sambhav.jain@live.com", "SDET-Lead");
-
         Response response= RestAssured.given().contentType(ContentType.JSON).body(user).log().all()
                 .post(BASE_URL + LIST_USER);
-
         response.then()
                 .assertThat()
                 .statusCode(201)
                 .and()
                     .assertThat()
                 .header("Content-Type", "application/json; charset=utf-8");
-
     }
 }
